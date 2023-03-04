dca9c54c6dd5b9b453b6678d67a51ac9770c9a97
TimOfey25Lebiaphan <73393865+TimOfey25Lebiaphan@users.noreply.github.com>
2023-03-04 12:58:43 +0300

    Add-drawing-functionality-inmainwindow-not-ideal
    C наш, сущий в памяти!
    да компилируется код Твой;
    да приидет царствие Софта Твоего;
    да будут действительны указатели Твои
    и в ОЗУ, как на жестком диске;
    массив наш насущный подавай нам на каждый день;
    и прости нам варнинги наши,
    как и мы избавляемся от ошибок наших;
    и не введи нас в бесконечный цикл,
    но избавь нас от винды.
    Ибо Твое есть Царство и сила и слава во веки.
    Энтер.
    K&R.16:64:256:1—8

--- DiplomaProrotype/MainWindow.xaml.cs
+++ DiplomaProrotype/MainWindow.xaml.cs
@@ -9,10 +9,12 @@ using System.Reflection;
 using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -568,7 +570,191 @@ namespace DiplomaProrotype
                 ObjectPanel.Children.Add(tile);
             }
         }
-        #endregion 
+        #endregion
+
+
+        public lineControl Line;
+        public int Mode = 1;
+
+        public int Delay = 500;
+        public int Speed = 1500;
+
+        private List<lineControl> _lines = new List<lineControl>();
+        private System.Threading.Thread _animationThread;
+
+        private void StartAnimation(object sender = null, RoutedEventArgs e = null)
+        {
+
+            StopAnimation();
+            TargetCanvas.IsEnabled = false;
+
+            lineControl.CompleteAnimationCallback callback = delegate (lineControl l)
+            {
+
+                Task.Run(delegate ()
+                {
+                    _animationThread = System.Threading.Thread.CurrentThread; //берём поток нащей функии чтобы потом в любой момент убить его
+                    foreach (lineControl line in _lines)
+                    {
+                        if (_animationThread == null) return;
+                        System.Threading.Thread.Sleep(Delay); //задержка
+                        App.Current.Dispatcher.Invoke(delegate ()
+                        {
+                            line.SetSpeed(new TimeSpan(0, 0, 0, 0, Speed)); //установка скорости
+                            line.BeginAnimation(); //старт анимации
+                        });
+
+                    }
+                });
+            };
+
+            _lines.Last().CompleteAnimationEvent += callback; //подписиваемся на событие
+
+            foreach (lineControl line in _lines)
+            {
+                line.line.X1 = line.line.X2 = line.line.Y1 = line.line.Y2 = 0; //скрываем линии
+            }
+            callback(null);
+
+        }
+
+        private void StopAnimation(object sender = null, RoutedEventArgs e = null)
+        {
+            _animationThread?.Abort(); //убиваем поток если он есть
+            _animationThread = null;
+
+            foreach (lineControl line in _lines) //убираем функции с события и останавливаем анимации
+            {
+                line.StopAnimation();
+                line.RemoveAllHandles_CompleteAnimationEvent();
+            }
+            TargetCanvas.IsEnabled = true;
+        }
+
+        private void ClearCanvas(object sender, RoutedEventArgs e)
+        {
+            _animationThread?.Abort(); //убиваем поток если он есть
+            _animationThread = null;
+
+            foreach (lineControl line in _lines) //убираем функции с события и останавливаем анимации
+            {
+                line.StopAnimation();
+                line.RemoveAllHandles_CompleteAnimationEvent();
+            }
+            TargetCanvas.IsEnabled = true;
+
+            _lines.Clear();
+            TargetCanvas.Children.Clear();
+        }
+
+        private void SetLinePosition(int mode)
+        {
+            if (Line == null) return;
+            switch (mode)
+            {
+                case 1: //стандарт
+                    Line.X2 = Mouse.GetPosition(this).X - TargetCanvas.Margin.Left;
+                    Line.Y2 = Mouse.GetPosition(this).Y - TargetCanvas.Margin.Top;
+                    break;
+
+                case 2: //по горизонтали
+                    Line.X2 = Mouse.GetPosition(this).X - TargetCanvas.Margin.Left;
+                    Line.Y2 = Line.Y1;
+                    break;
+
+                case 3: //по вертикали
+                    Line.X2 = Line.X1;
+                    Line.Y2 = Mouse.GetPosition(this).Y - TargetCanvas.Margin.Top;
+                    break;
+
+                case 4: //по углом 45 градусов слева
+                    Line.X2 = Mouse.GetPosition(this).X - TargetCanvas.Margin.Left;
+                    Line.Y2 = Line.Y1 - (Line.X1 - Mouse.GetPosition(this).X + TargetCanvas.Margin.Left);
+                    break;
+
+                case 5: //по углом 45 градусов справа
+                    Line.X2 = Mouse.GetPosition(this).X - TargetCanvas.Margin.Left;
+                    Line.Y2 = Line.Y1 + (Line.X1 - Mouse.GetPosition(this).X + TargetCanvas.Margin.Left);
+                    break;
+            }
+            //Console.WriteLine($"X1: {l.X1}, X2: {l.X2}, Y1: {l.Y1}, Y2: {l.Y2}, V: {(l.X1 - Mouse.GetPosition(this).X)}"); //DEBUG
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            SetLinePosition(Mode); //обновляем линию
+        }
+
+        private void Canvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Line = new lineControl()
+            {
+                X1 = (Line == null) ? Mouse.GetPosition(this).X - TargetCanvas.Margin.Left : Line.X2,
+                Y1 = (Line == null) ? Mouse.GetPosition(this).Y - TargetCanvas.Margin.Top : Line.Y2,
+                X2 = Mouse.GetPosition(this).X - TargetCanvas.Margin.Left,
+                Y2 = Mouse.GetPosition(this).Y - TargetCanvas.Margin.Top
+            };
+            Line.line.StrokeThickness = 3; //ширина линии
+
+            _lines.Add(Line);
+            TargetCanvas.Children.Add(Line);
+
+        }
+
+        private void Canvas_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            SetLinePosition(Mode);
+            Line = null;
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Source.GetType() == typeof(TextBox) || (!Keyboard.IsKeyDown(Key.LeftCtrl) && !Keyboard.IsKeyDown(Key.RightCtrl) &&
+                    !Keyboard.IsKeyDown(Key.Tab))) return; //проверка на Ctrl и Tab
+                Mode = Convert.ToInt32(e.Key.ToString()[1].ToString());
+                ModeChange_Click(modes_grid.Children[Mode - 1]);
+            }
+            catch (Exception)
+            { }
+            finally
+            {
+                Canvas_MouseMove(null, null);
+            }
+        }
+
+        private void TB_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (sender.GetType() != typeof(TextBox)) return;
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void Delay_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Delay = ((sender as TextBox).Text == "") ? 0 : Convert.ToInt32((sender as TextBox).Text);
+        }
+
+        private void Speed_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Speed = ((sender as TextBox).Text == "") ? 0 : Convert.ToInt32((sender as TextBox).Text);
+        }
+
+        private void ModeChange_Click(object sender, RoutedEventArgs e = null)
+        {
+            ToggleButton s = sender as ToggleButton;
+            UniformGrid parent = s.Parent as UniformGrid;
+
+            foreach (ToggleButton button in parent.Children)
+            {
+                button.IsChecked = false;
+            }
+            s.IsChecked = true;
+
+            Mode = Convert.ToInt32(s.Tag);
+            Canvas_MouseMove(null, null);
+        }
 
     }
 }
